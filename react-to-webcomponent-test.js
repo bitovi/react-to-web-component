f020c61f7181ef28e299478474d2075c7f2887fd
justinbmeyer <justinbmeyer@gmail.com>
2019-08-15 17:29:53 -0500

    gets the basics to work

---
+++ react-to-webcomponent-test.js
@@ -0,0 +1,72 @@
+import QUnit from "steal-qunit";
+import React from 'react';
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+
+import reactToWebComponent from "./react-to-webcomponent";
+
+QUnit.module("react-to-webcomponent");
+
+
+QUnit.test("basics with react", function(assert){
+    class Welcome extends React.Component {
+      render() {
+        return <h1>Hello, {this.props.name}</h1>;
+      }
+    }
+
+    class MyWelcome extends reactToWebComponent(Welcome,React, ReactDOM) {}
+
+    customElements.define("my-welcome", MyWelcome);
+
+    var fixture = document.getElementById("qunit-fixture");
+
+    var myWelcome = new MyWelcome();
+    fixture.appendChild(myWelcome);
+
+    assert.equal(myWelcome.nodeName, "MY-WELCOME", "able to read nodeName");
+
+    assert.equal(myWelcome.childNodes.length, 1, "able to render something")
+
+    assert.equal(myWelcome.childNodes[0].innerHTML, "Hello, " ,"renders the right thing");
+
+    myWelcome.name = "Justin";
+
+    assert.equal(myWelcome.childNodes[0].innerHTML, "Hello, Justin", "can update");
+
+});
+
+QUnit.test("works with attributes set with propTypes", function(assert){
+    class Greeting extends React.Component {
+      render() {
+        return <h1>Hello, {this.props.name}</h1>;
+      }
+    }
+    Greeting.propTypes = {
+      name: PropTypes.string.isRequired
+    };
+
+    class MyGreeting extends reactToWebComponent(Greeting,React, ReactDOM) {}
+
+    customElements.define("my-greeting", MyGreeting);
+
+    var fixture = document.getElementById("qunit-fixture");
+
+    var myGreeting = new MyGreeting();
+
+    var oldError = console.error;
+    console.error = function(message){
+        assert.ok(message.includes("required"), "got a warning with required");
+        oldError = console.error;
+    }
+    fixture.appendChild(myGreeting);
+
+
+
+    fixture.innerHTML = "<my-greeting name='Christopher'></my-greeting>";
+
+    assert.equal(fixture.firstElementChild.innerHTML,"<h1>Hello, Christopher</h1>");
+
+
+
+});
---
+++ react-to-webcomponent.js
@@ -0,0 +1,75 @@
+var reactComponentSymbol = Symbol.for("r2wc.reactComponent");
+var renderSymbol = Symbol.for("r2wc.reactRender");
+
+var define = {
+	// Creates a getter/setter that re-renders everytime a property is set.
+	expando: function(receiver, key, value) {
+		Object.defineProperty(receiver, key, {
+			enumerable: true,
+			get: function() {
+				return value;
+			},
+			set: function(newValue) {
+				value = newValue;
+				this[renderSymbol]();
+				return true;
+			}
+		});
+		receiver[renderSymbol]();
+	}
+}
+
+module.exports = function(ReactComponent, React, ReactDOM) {
+
+	// Create the web component "class"
+	var WebComponent = function() {
+		var self = Reflect.construct(HTMLElement, arguments, this.constructor);
+		return self;
+	};
+
+
+	// Make the class extend HTMLElement
+	var targetPrototype = Object.create(HTMLElement.prototype);
+	targetPrototype.constructor = WebComponent;
+
+	// But have that prototype be wrapped in a proxy.
+	var proxyPrototype = new Proxy(targetPrototype, {
+		// when any undefined property is set, create a getter/setter that re-renders
+		set: function(target, key, value, receiver) {
+			if (typeof key === "symbol" || key === "_reactRootContainer" || key in target) {
+				return Reflect.set(target, key, value, receiver);
+			} else {
+				define.expando(receiver, key, value)
+			}
+			return true;
+		}
+	});
+	WebComponent.prototype = proxyPrototype;
+
+	// Setup lifecycle methods
+	targetPrototype.connectedCallback = function() {
+		this[renderSymbol]();
+	};
+	targetPrototype[renderSymbol] = function() {
+		if (this.isConnected) {
+			var data = {};
+			Object.keys(this).forEach(function(key) {
+				if (key !== "_reactRootContainer") {
+					data[key] = this[key];
+				}
+			}, this);
+			this[reactComponentSymbol] = ReactDOM.render(React.createElement(ReactComponent, data), this)
+		}
+	};
+
+	// Handle attributes changing
+	if (ReactComponent.propTypes) {
+		WebComponent.observedAttributes = Object.keys(ReactComponent.propTypes);
+		targetPrototype.attributeChangedCallback = function(name, oldValue, newValue) {
+			// TODO: handle type conversion
+			this[name] = newValue;
+		}
+	}
+
+	return WebComponent;
+}
