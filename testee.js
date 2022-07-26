2ce1aa4dd42758d8b5f9eaf380d092e7feb5be23
janebitovi <jane@bitovi.com>
2022-07-26 15:01:16 -0400

    restore postinstall script and fix tests

--- testee.js
+++ testee.js
@@ -1,14 +1,14 @@
 module.exports = {
-    browsers: [
-        {
-            clean: true,
-            browser: "chrome",
-            args: [
-                "--headless",
-                "--disable-gpu",
-                "--no-sandbox",
-                "--remote-debugging-port=9222"
-            ],
-        },
-    ],
+  browsers: [
+    {
+      clean: true,
+      browser: "chrome",
+      args: [
+        "--headless",
+        "--disable-gpu",
+        "--no-sandbox",
+        "--remote-debugging-port=9222"
+      ],
+    },
+  ],
 };
