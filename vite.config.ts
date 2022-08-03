d323058996494ed64620890fdeb97a2b79465eb1
janebitovi <jane@bitovi.com>
2022-08-03 10:20:47 -0400

    wip use vite-plugin-dts instead of ts rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import typescript from 'rollup-plugin-typescript2'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig((configEnv) => ({
-  plugins: [typescript()],
+  plugins: [dts({ insertTypesEntry: true })],
   build: {
     lib: {
       formats: ['es', 'cjs', 'umd'],
-      entry: resolve(__dirname, 'src/react-to-webcomponent.js'),
+      entry: resolve(__dirname, 'src/index.js'),
       name: 'react-to-webcomponent',
       fileName: (format) => `react-to-webcomponent.${format}.js`,
     },
