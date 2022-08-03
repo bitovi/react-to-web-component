fdd3876bb27696605623452315d63a0a4f303694
David Nicholas <david_nicholas@apple.com>
2022-08-03 08:08:15 -0700

    updated build and plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import typescript from "rollup-plugin-typescript2"
 
 export default defineConfig((configEnv) => ({
-  plugins: [dts({ insertTypesEntry: true })],
+  plugins: [typescript()],
   build: {
     lib: {
-      formats: ['es', 'cjs', 'umd'],
-      entry: resolve(__dirname, 'src/index.js'),
-      name: 'react-to-webcomponent',
+      formats: ["es", "cjs", "umd"],
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "react-to-webcomponent",
       fileName: (format) => `react-to-webcomponent.${format}.js`,
     },
     rollupOptions: {
-      external: ['react'],
+      external: ["react"],
       output: {
         globals: {
-          react: 'react',
+          react: "react",
         },
       },
     },
