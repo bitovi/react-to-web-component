fe52df9b859cd1af949228c716af99f898ada09c
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2023-04-27 09:22:41 -0500

    add more core tests

--- src/core/core.test.tsx
+++ src/core/core.test.tsx
@@ -1,6 +1,9 @@
+import matchers from "@testing-library/jest-dom/matchers"
 import { describe, test, expect, vi, afterEach } from "vitest"
 import React from "react"
 import r2wc from "."
+
+expect.extend(matchers)
 
 // mock renderer
 const mount = vi.fn()
@@ -108,18 +111,31 @@ describe("r2wc core", () => {
       funcProp: () => void
     }
 
-    function ButtonWithDifferentPropTypes({text, numProp, boolProp, arrProp, objProp, funcProp}: Props) {
+    function ButtonWithDifferentPropTypes({
+      text,
+      numProp,
+      boolProp,
+      arrProp,
+      objProp,
+      funcProp,
+    }: Props) {
       return <button>{text}</button>
     }
 
-    const ButtonElement = r2wc(ButtonWithDifferentPropTypes, {props: {
-      text: String,
-      numProp: Number,
-      boolProp: Boolean,
-      arrProp: Array,
-      objProp: Object,
-      funcProp: Function
-    }}, { mount, unmount, onUpdated })
+    const ButtonElement = r2wc(
+      ButtonWithDifferentPropTypes,
+      {
+        props: {
+          text: String,
+          numProp: Number,
+          boolProp: Boolean,
+          arrProp: Array,
+          objProp: Object,
+          funcProp: Function,
+        },
+      },
+      { mount, unmount, onUpdated },
+    )
 
     global.globalFn = function () {
       expect(true).toBe(true)
@@ -132,7 +148,9 @@ describe("r2wc core", () => {
     body.innerHTML = `<test-button-element-property text='hello' obj-prop='{"greeting": "hello, world"}' arr-prop='["hello", "world"]' num-prop='240' bool-prop='true' func-prop='globalFn'>
                       </test-button-element-property>`
 
-    const testEl = body.querySelector("test-button-element-property") as HTMLElement & Props
+    const testEl = body.querySelector(
+      "test-button-element-property",
+    ) as HTMLElement & Props
 
     await flushPromises()
 
@@ -140,7 +158,7 @@ describe("r2wc core", () => {
     expect(testEl.numProp).toBe(240)
     expect(testEl.boolProp).toBe(true)
     expect(testEl.arrProp).toEqual(["hello", "world"])
-    expect(testEl.objProp).toEqual({greeting: "hello, world"})
+    expect(testEl.objProp).toEqual({ greeting: "hello, world" })
     expect(testEl.funcProp).toBeInstanceOf(Function)
     expect(testEl.funcProp()).toBe(true)
 
@@ -154,6 +172,35 @@ describe("r2wc core", () => {
     expect(testEl.getAttribute("text")).toBe("world")
     expect(testEl.getAttribute("num-prop")).toBe("100")
     expect(testEl.getAttribute("bool-prop")).toBe("false")
+  })
 
+  test("sets HTML property not defined in props but found on HTML object", async () => {
+    function Button({ text = "Hello, button" }: { text: string }) {
+      return <button>{text}</button>
+    }
+
+    const ButtonElement = r2wc(
+      Button,
+      { props: ["text"] },
+      { mount, unmount, onUpdated },
+    )
+
+    customElements.define("test-button-element-non-prop", ButtonElement)
+
+    const body = document.body
+    body.innerHTML = `<test-button-element-non-prop></test-button-element-non-prop>`
+
+    const testEl = body.querySelector(
+      "test-button-element-non-prop",
+    ) as HTMLElement & { text: string }
+    testEl.style.backgroundColor = "red"
+    testEl.style.visibility = "hidden"
+    testEl.id = "test-button-id"
+
+    await flushPromises()
+
+    expect(testEl).toHaveStyle("background-color: red;")
+    expect(testEl).not.toBeVisible()
+    expect(body.querySelector("#test-button-id")).toBe(testEl)
   })
 })
