873a57a81b9e3b05636aefe29cffa68fe46eaec3
Christopher J Baker <christopher@bitovi.com>
2023-05-12 10:40:32 -0700

    update react-to-web-component

--- packages/react-to-web-component/src/react-to-web-component.ts
+++ packages/react-to-web-component/src/react-to-web-component.ts
@@ -1,14 +1,43 @@
 import type { R2WCOptions } from "@r2wc/core"
 
+import React from "react"
 import ReactDOM from "react-dom"
 
 import r2wcCore from "@r2wc/core"
 
-function mount(container: HTMLElement, element: JSX.Element) {
+interface Context<Props extends React.Attributes> {
+  container: HTMLElement
+  ReactComponent: React.ComponentType<Props>
+}
+
+function mount<Props extends React.Attributes>(
+  container: HTMLElement,
+  ReactComponent: React.ComponentType<Props>,
+  props: Props,
+): Context<Props> {
+  const element = React.createElement(ReactComponent, props)
+
+  ReactDOM.render(element, container)
+
+  return {
+    container,
+    ReactComponent,
+  }
+}
+
+function update<Props extends React.Attributes>(
+  { container, ReactComponent }: Context<Props>,
+  props: Props,
+): void {
+  const element = React.createElement(ReactComponent, props)
+
   ReactDOM.render(element, container)
 }
 
-function unmount(container: HTMLElement) {
+function unmount<Props extends React.Attributes>(
+  { container }: Context<Props>,
+  props: Props,
+): void {
   ReactDOM.unmountComponentAtNode(container)
 }
 
@@ -16,5 +45,5 @@ export default function r2wc(
   ReactComponent: React.FC<any> | React.ComponentClass<any>,
   config: R2WCOptions = {},
 ): CustomElementConstructor {
-  return r2wcCore(ReactComponent, config, { mount, unmount })
+  return r2wcCore(ReactComponent, config, { mount, unmount, update })
 }
